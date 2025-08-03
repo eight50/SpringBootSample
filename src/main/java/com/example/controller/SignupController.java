b5b72216372529b0a90965ba93df8e08a77238ee
eight50 <eight2159@gmail.com>
2025-08-03 17:22:45 +0900

    バインド、エラー処理追加

--- src/main/java/com/example/controller/SignupController.java
+++ src/main/java/com/example/controller/SignupController.java
@@ -6,14 +6,20 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.application.service.UserApplicationService;
+import com.example.form.SignupForm;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("/user") // URLのプリフィックス(接頭辞)になる。例：localhost:8080/user/になる
+@Slf4j // @Slf4jはLombokのアノテーションで、ログ出力
 public class SignupController {
 	@Autowired
 	private UserApplicationService userApplicationService;
@@ -21,10 +27,12 @@ public class SignupController {
 	/**
 	 * ユーザ登録画面を表示
 	 * @param model
+	 * @param locale
+	 * @param form @ModelAttribute: model.Attribute("signupForm", form)と同じ意味
 	 * @return ユーザ登録画面のテンプレート名（.html）
 	 */
 	@GetMapping("/signup")
-	public String getSignup(Model model, Locale locale) {
+	public String getSignup(Model model, Locale locale, @ModelAttribute SignupForm form) {
 		// 性別を取得
 		Map<String, Integer> genderMap = userApplicationService.getGenderMap(locale);
 		model.addAttribute("genderMap", genderMap);
@@ -35,8 +43,15 @@ public class SignupController {
 	/**
 	 * ユーザ登録処理
 	 */
-	@PostMapping("/singup")
-	public String postSignup() {
+	@PostMapping("/signup")
+	public String postSignup(Model model, Locale locale, @ModelAttribute SignupForm form, BindingResult bindingResult) {
+		// 入力チェック
+		if(bindingResult.hasErrors()) {
+			// True -> エラー有り、ユーザー登録画面に戻る
+			return getSignup(model, locale, form);
+		}
+		
+		log.info(form.toString());
 		// ログイン画面にリダイレクト
 		// ユーザ登録した後、他の画面をGet（POST-Redirect-GET:PRG）して、誤った操作や、画面更新による二重登録を防止
 		return "redirect:/login";
---
+++ src/main/java/com/example/form/SignupForm.java
@@ -0,0 +1,23 @@
+package com.example.form;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+/**
+ * 画面の入力内容とJavaのクラスをマッピング（バインド）するクラス
+ */
+@Data
+public class SignupForm {
+	private String userId;
+	private String password;
+	private String userName;
+	
+	@DateTimeFormat(pattern="yyyy/MM/dd")
+	private Date birthday;
+	
+	private Integer age;
+	private Integer gender;
+}
