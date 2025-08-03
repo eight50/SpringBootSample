76bc9f66a238321f3468bf0bb920a4444a0244e0
eight50 <eight2159@gmail.com>
2025-08-03 15:46:17 +0900

    多言語化、propertyファイル作成

--- src/main/java/com/example/controller/SignupController.java
+++ src/main/java/com/example/controller/SignupController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import java.util.Locale;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,9 +24,9 @@ public class SignupController {
 	 * @return ユーザ登録画面のテンプレート名（.html）
 	 */
 	@GetMapping("/signup")
-	public String getSignup(Model model) {
+	public String getSignup(Model model, Locale locale) {
 		// 性別を取得
-		Map<String, Integer> genderMap = userApplicationService.getGenderMap();
+		Map<String, Integer> genderMap = userApplicationService.getGenderMap(locale);
 		model.addAttribute("genderMap", genderMap);
 		
 		return "user/signup";
